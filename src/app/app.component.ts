2bfda76478c1ef7b5fcaf165b5a3dd493b63c830
Vasilij Zhukov <v.zukovs@dynatech.lv>
2018-05-15 14:53:14 +0300

    Allow resources in components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import {ChangeDetectionStrategy, ChangeDetectorRef, Component, OnDestroy} from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class AppComponent {
+export class AppComponent implements OnDestroy {
   title = 'app';
+  isResourceLoaded = false;
+  private resourceTimeout = null;
+
+  constructor(private changeDetector: ChangeDetectorRef) {
+  }
+
+  handleResourceLoad() {
+    this.resourceTimeout = setTimeout(() => {
+      this.isResourceLoaded = true;
+      this.changeDetector.detectChanges();
+    }, 1000);
+  }
+
+  ngOnDestroy() {
+    if (this.resourceTimeout) {
+      clearTimeout(this.resourceTimeout);
+    }
+  }
 }
